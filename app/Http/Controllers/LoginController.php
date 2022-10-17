e93fe47801c082a6ae357ab8649767d0b5a10e31
kamotchy <115168183+kamotchy@users.noreply.github.com>
2022-10-18 00:50:57 +0800

    Implement authentication logic

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            if (Auth::id() == 1) {
+                return redirect()->intended('admin');
+            }
+
+            return redirect()->intended('dashboard');
+        }
+    }
+}
